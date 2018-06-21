349eed32f7ea29f96943616c8b269c2d47753d35
Hristo Hristov <hristo_lv96@abv.bg>
2018-06-21 19:21:59 +0300

    Apache Complex
    Implements the use of Apache's Complex [number] class. Slower than non- complex version

--- src/Fractal.java
+++ src/Fractal.java
@@ -2,28 +2,35 @@ import java.awt.Color;
 import java.io.File;
 import java.awt.image.BufferedImage;
 import javax.imageio.ImageIO;
+import org.apache.commons.math3.complex.Complex;
 
 public class Fractal {
     public static void main(String[] args) throws Exception {
-        int width = 1920, height = 1080, maxIterations = 1000;
+        int width = 1920, height = 1080, maxIterations = 50;
         BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         int[] colorPalette = new int[maxIterations];
         for (int i = 0; i<maxIterations; i++) {
             colorPalette[i] = Color.HSBtoRGB(i/256f, 1, i/(i+8f));
         }
 
+        long startTime = System.nanoTime();
+
         for (int row = 0; row < height; row++) {
+            System.out.println("Progress: " + row + "/" + height);
             for (int col = 0; col < width; col++) {
                 double cReal = (col - width / 2) * 4.0 / width;
                 double cImaginary = (row - height / 2) * 4.0 / width;
-                double x = 0, y = 0;
-                double xNew;
-                int iterationCounter = 0;
+                Complex C = new Complex(cReal, cImaginary);
+                Complex Z = new Complex(0, 0);
+                Complex e = new Complex(Math.E, 0);
 
-                while (x * x + y * y < 4 && iterationCounter < maxIterations) {
-                    xNew = x * x - y * y + cReal;
-                    y = 2 * x * y + cImaginary;
-                    x = xNew;
+                int iterationCounter = 0;
+                while (Z.getReal() * Z.getReal() + Z.getImaginary() * Z.getImaginary() < 4 && iterationCounter < maxIterations) {
+
+                    if(Z.getReal() != 0 || Z.getImaginary() != 0){
+                        Z = Z.multiply(Z);
+                    }
+                    Z = Z.add(C);
                     iterationCounter++;
                 }
                 if (iterationCounter < maxIterations){
@@ -34,6 +41,7 @@ public class Fractal {
             }
         }
 
+        System.out.println("Elapsed time: " + ((System.nanoTime() - startTime)/1000000000.0));
         ImageIO.write(image, "png", new File("mandelbrot2.png"));
     }
 }
