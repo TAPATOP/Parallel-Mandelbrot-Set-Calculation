c4bfaa2bc15b68e2476f3e0708ccf4aa4bc32ce8
Hristo Hristov <hristo_lv96@abv.bg>
2018-06-22 12:08:30 +0300

    Variable implementation
    Implements the capability of affecting the fractal's display by incorporating appropriate variables in the calculations

--- src/Fractal.java
+++ src/Fractal.java
@@ -6,31 +6,40 @@ import org.apache.commons.math3.complex.Complex;
 
 public class Fractal {
     public static void main(String[] args) throws Exception {
-        int width = 1920, height = 1080, maxIterations = 50;
-        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        long startTime = System.nanoTime();
+        int maxIterations = 20;
         int[] colorPalette = new int[maxIterations];
         for (int i = 0; i<maxIterations; i++) {
             colorPalette[i] = Color.HSBtoRGB(i/256f, 1, i/(i+8f));
         }
+        int width = 1920;
+        int height = 1080;
+        double resolutionProportion = (double)height / width;
+        double xLimitLow = 0;
+        double xLimitHigh = 2;
+        double yLimitLow = 0;
+        double yLimitHigh = 2;
+        double verticalStepBetweenPixels = (xLimitHigh - xLimitLow) / width;
+        double horizontalStepBetweenPixels = (yLimitHigh - yLimitLow) / height;
+        int calculationLimit = 16;
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
-        long startTime = System.nanoTime();
-
+        double cImaginary;
+        double cReal;
         for (int row = 0; row < height; row++) {
-            System.out.println("Progress: " + row + "/" + height);
+            System.out.println("Progress: " + (row + 1) + "/" + height);
+            cImaginary = (yLimitLow + row * horizontalStepBetweenPixels) * resolutionProportion;
+
             for (int col = 0; col < width; col++) {
-                double cReal = (col - width / 2) * 4.0 / width;
-                double cImaginary = (row - height / 2) * 4.0 / width;
+                cReal = xLimitLow + col * verticalStepBetweenPixels;
                 Complex C = new Complex(cReal, cImaginary);
                 Complex Z = new Complex(0, 0);
                 Complex e = new Complex(Math.E, 0);
 
                 int iterationCounter = 0;
-                while (Z.getReal() * Z.getReal() + Z.getImaginary() * Z.getImaginary() < 4 && iterationCounter < maxIterations) {
-
-                    if(Z.getReal() != 0 || Z.getImaginary() != 0){
-                        Z = Z.multiply(Z);
-                    }
-                    Z = Z.add(C);
+                while (Z.getReal() * Z.getReal() + Z.getImaginary() * Z.getImaginary() < calculationLimit &&
+                        iterationCounter < maxIterations) {
+                    Z = C.multiply(Z.cos());
                     iterationCounter++;
                 }
                 if (iterationCounter < maxIterations){
@@ -42,6 +51,6 @@ public class Fractal {
         }
 
         System.out.println("Elapsed time: " + ((System.nanoTime() - startTime)/1000000000.0));
-        ImageIO.write(image, "png", new File("mandelbrot2.png"));
+        ImageIO.write(image, "png", new File("mandelbrot3.png"));
     }
 }
